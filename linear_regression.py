835d0a45630d6d7b8cfe097ad6be44390cda3d30
Jackson Small <132709056+jacksonSmall@users.noreply.github.com>
2023-07-06 09:48:14 -0400

    update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -5,65 +5,49 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 
-# asks and creates x - values
+def ask_for_x():
+    while True:
+        input_number_x = input("x value: ")
+        try:
+            return float(input_number_x)
+        except ValueError:
+            print("That is not a number. Please enter a number. ")
 
-print("------ x values ------")
+def ask_for_y():
+    while True:
+        input_number_y = input("y value: ")
+        try:
+            return float(input_number_y)
+        except ValueError:
+            print("That is not a number. Please enter a number. ")
+
 numbers_x = []
-
-
-while True:
-    input_number_x = input("Please enter a number or enter 'q' to quit: ")
-
-    if input_number_x.lower() == 'q':
-        break
-    try:
-        input_number_x = float(input_number_x)
-        numbers_x.append(input_number_x)
-    except ValueError:
-        print("That is not a number. Please enter a number. ")
-
-    x_val = np.array(numbers_x)
-
-print("X - Values:", x_val, sep="\n")
-
-# asks and creates y - values
-
-
-print("------ y values ------")
 numbers_y = []
 
-while True:
-    input_number_y = input("Please enter a number or enter 'q' to quit: ")
+pts_len = int(input('How many points do u wanna put in????????????'))
 
-    if input_number_y.lower() == 'q':
-        break
-    try:
-        input_number_y = float(input_number_y)
-        numbers_y.append(input_number_y)
-    except ValueError:
-        print("That is not a number. Please enter a number. ")
-
-    y_val = np.array(numbers_y)
-
-print("Y - Values:", y_val, sep="\n")
-
-# tests shape
-
-print(np.shape(x_val))
-print(np.shape(y_val))
-
-# tests dimensions
-
-# print(x_val.ndim)
-# print(y_val.ndim)
-
-# Linear Regression Analysis
-if np.shape(x_val) == np.shape(y_val) and x_val.ndim == y_val.ndim:
-    plt.plot(x_val, y_val)
-    plt.show()
-    print("Opening graph...")
-elif np.shape(x_val) != np.shape(y_val) or x_val.ndim != y_val.ndim:
-    print("The number of x's and y's don't match! Please correct them.")
+for i in range(pts_len):
+    print("Please enter a (x,y) pair ")
+    numbers_x.append(ask_for_x())
+    numbers_y.append(ask_for_y())
 
 
+x_val = np.array(numbers_x)
+print("X - Values:", x_val, sep="\n")
+y_val = np.array(numbers_y)
+print("y - Values:", y_val, sep="\n")
+
+plt.scatter(x_val, y_val)
+plt.show()
+plt.plot(x_val, y_val)
+plt.show()
+
+out_arr = np.add(x_val, y_val)
+
+sns.histplot(data=x_val, kde=True, color= 'skyblue')
+plt.xlabel("x values")
+plt.ylabel("Frequency")
+plt.title("x value histogram")
+plt.show()
